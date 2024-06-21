c596f9a7c26b32b008233bf100d81eda62058db4
mwuweb <166729267+mwuweb@users.noreply.github.com>
2024-06-21 13:51:31 +0900

    jsonの作成

--- js-template/career.js
+++ js-template/career.js
@@ -10,19 +10,19 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   const res = await fetch("career.json"); //fetch関数を使ってdata.jsonを取得している
   const obj = await res.json(); //取得したレスポンスをJSON形式で解析し、resからobjにJSONデータを格納している
-  const career = obj.care; //obj内のlistに含まれるデータをdataに格納している
-  console.log(career);
+  const data = obj.list; //obj内のlistに含まれるデータをdataに格納している
+  console.log(data);
   /*ウェブページがロードされたらサーバーから取得したJSON形式のデータを使って動的なコンテンツを生成するための処理を行う*/
 
   document
     .querySelectorAll("span.unread")
-    .forEach((el) => (el.textContent = career.length)); //span要素がunreadの要素を選択し。その要素のテキストコンテンツをdata.lengthに設定
+    .forEach((el) => (el.textContent = data.length)); //span要素がunreadの要素を選択し。その要素のテキストコンテンツをdata.lengthに設定
   /*未読の件数を表示するためにページ内のすべての未読要素に未読データの長さを設定している*/
 
-  const career_list = document.querySelector("div#career_list");
+  const info_list = document.querySelector("div#info_list");
   //ページ内のdiv要素を取得しinfo_listに代入しjs内で操作できるようにしている
 
-  for (const item of career) {
+  for (const item of data) {
     const record = document.createElement("div"); //div要素を生成し、record変数(各データ項目の表示を囲むコンテナ)に代入
     record.className = "record";
     for (const [prop, val] of Object.entries(item)) {
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       }
       //取得したデータを元にして動的なHTML要素を生成し、ページに表示するための処理を行っている
 
-      if (prop == "sch") {
+      if (prop == "place") {
         const tri = document.createElement("div"); //div要素triを生成
         tri.textContent = "&nbsp;"; //&nbsp(スペース)を設定
         tri.className = "tri"; //triクラスを追加
@@ -51,6 +51,6 @@ document.addEventListener("DOMContentLoaded", async () => {
       } //subjectプロパティが存在するデータ項目に対して特定の表示を追加
       record.appendChild(el); //el要素をrecord要素に追加
     }
-    career_list.appendChild(record); //record要素をinfo_listに追加
+    info_list.appendChild(record); //record要素をinfo_listに追加
   }
 });
