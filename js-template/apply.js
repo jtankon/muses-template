2d57d8784d552c3122668553081bf5c7fe6dee49
averdak <166770926+averdak@users.noreply.github.com>
2024-07-12 12:17:39 +0900

    apply画面

--- js-template/apply.js
+++ js-template/apply.js
@@ -20,12 +20,12 @@ document.addEventListener("DOMContentLoaded", async () => {
     eventDiv.innerHTML = `
       <div class="event-header">${event.title}</div>
       <div class="event-body">
-        <p>内容: ${event.content}</p>
-        <p>開催日時: ${event.date}</p>
-        <p>会場: ${event.venue}</p>
-        <p>定員: ${event.capacity}</p>
-        <p>申込期限: ${event.deadline}</p>
-        <p>取扱窓口: ${event.contact}</p>
+        <p>内容：${event.content}</p>
+        <p>開催日時：${event.date}</p>
+        <p>種別：${event.venue}</p>
+        <p>定員：${event.capacity}</p>
+        <p>申込期限：${event.deadline}</p>
+        <p>取扱部署：${event.contact}</p>
       </div>
     `;
     eventsContainer.appendChild(eventDiv);
