881fd1f184a357e3896b1b2417b62cf534688cf1
mwuweb <166729267+mwuweb@users.noreply.github.com>
2024-07-12 18:57:00 +0900

    menuの修正

--- js-template/menu.js
+++ js-template/menu.js
@@ -1,55 +1,50 @@
-"use strict";
+'use strict';
 
-document.addEventListener("DOMContentLoaded", async () => {
+document.addEventListener('DOMContentLoaded', async () => {
   const username = sessionStorage.username;
   if (!username) {
-    window.alert("ログインしてください");
-    location.href = "login.html";
-  } //ユーザーネームを入力しなかったらアラートが出る
-  document.querySelector("#user_name span").textContent = username; //ユーザーネームの挿入
+    window.alert('ログインしてください');
+    location.href = 'login.html';
+  }
+  document.querySelector('#user_name span').textContent = username;
 
-  const res = await fetch("data.json"); //fetch関数を使ってdata.jsonを取得している
-  const obj = await res.json(); //取得したレスポンスをJSON形式で解析し、resからobjにJSONデータを格納している
-  const data = obj.list; //obj内のlistに含まれるデータをdataに格納している
+  const res = await fetch('data.json');
+  const obj = await res.json();
+  const data = obj.list;
   console.log(data);
-  /*ウェブページがロードされたらサーバーから取得したJSON形式のデータを使って動的なコンテンツを生成するための処理を行う*/
 
-  document
-    .querySelectorAll("span.unread")
-    .forEach((el) => (el.textContent = data.length)); //span要素がunreadの要素を選択し。その要素のテキストコンテンツをdata.lengthに設定
-  /*未読の件数を表示するためにページ内のすべての未読要素に未読データの長さを設定している*/
+  document.querySelectorAll('span.unread').forEach((el) => (el.textContent = data.length));
 
-  const info_list = document.querySelector("div#info_list");
-  //ページ内のdiv要素を取得しinfo_listに代入しjs内で操作できるようにしている
+  const info_list = document.querySelector('div#info_list');
 
   for (const item of data) {
-    const record = document.createElement("div"); //div要素を生成し、record変数(各データ項目の表示を囲むコンテナ)に代入
-    record.className = "record";
+    const record = document.createElement('div');
+    record.className = 'record';
     for (const [prop, val] of Object.entries(item)) {
-      const el = document.createElement("div"); //elは各データ項目を表示するための要素
-      if (prop == "from") {
-        el.innerHTML = val; //fromの場合HTML内容を設定
+      const el = document.createElement('div');
+      if (prop == 'from') {
+        el.innerHTML = val;
       } else {
-        el.textContent = val; //要素のテキスト内容の設定
-        el.className = prop; //要素にプロパティ名をクラス名として追加する。これは各データ項目を識別するためのクラス名
+        el.textContent = val;
       }
-      //取得したデータを元にして動的なHTML要素を生成し、ページに表示するための処理を行っている
+      el.className = prop;
 
-      if (prop == "subject") {
-        const tri = document.createElement("div"); //div要素triを生成
-        tri.textContent = "&nbsp;"; //&nbsp(スペース)を設定
-        tri.className = "tri"; //triクラスを追加
-        record.appendChild(tri); //triをrecordに追加
+      if (prop == 'subject') {
+        const tri = document.createElement('div');
+        tri.textContent = '&nbsp;';
+        tri.className = 'tri';
+        record.appendChild(tri);
 
-        const mark = document.createElement("div");
-        mark.className = "mark";
-        const span = document.createElement("span");
-        span.textContent = "!";
-        span.className = "exmark";
+        const mark = document.createElement('div');
+        mark.className = 'mark';
+        const span = document.createElement('span');
+        span.textContent = '!';
+        span.className = 'exmark';
         mark.appendChild(span);
         record.appendChild(mark);
-      } //subjectプロパティが存在するデータ項目に対して特定の表示を追加
-      record.appendChild(el); //el要素をrecord要素に追加
+      }
+      record.appendChild(el);
     }
+    info_list.appendChild(record);
   }
 });
